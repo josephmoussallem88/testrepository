d418ea8778a2944c22ee48db07d8ef52a99691ad
josephmoussallem88 <josephmoussallem@live.com>
2024-06-25 10:46:31 +0200

    copy to new branch

--- testrepository/_modules/network/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "aws_network_interface" {
-    value = aws_network_interface.Web-server-nic.id
-}
-
-output "security_groups" {
-    value = aws_security_group.allow_web.id
-}
--- testrepository/_modules/network/var.tf
+++
@@ -1,9 +0,0 @@
-variable "availability_zone" {
-type= string
-default = "eu-west-3a"
-}
-
-variable "subnet1-tag" {
-type= string
-default = "production-Subnet"
-}
